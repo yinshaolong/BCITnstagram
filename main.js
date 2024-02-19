3a3f98ed8517ff3d6215145eabaff8e3e6adb767
omgitsjohn <syin18@my.bcit.ca>
2024-02-18 22:13:26 -0800

    user input working

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require('fs');
 const wt = require("worker-thread");
+const {AutoComplete} = require('enquirer');
 /*
  * Project: Milestone 1
  * File Name: main.js
@@ -14,11 +15,13 @@ const wt = require("worker-thread");
 const IOhandler = require("./IOhandler");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
-const pathProcessed = path.join(__dirname, "grayscaled");
+const pathProcessedGray = path.join(__dirname, "grayscaled");
+const pathProcessedSepia = path.join(__dirname, "sepia");
+const readline = require('readline');
 
 console.log(zipFilePath)
 console.log(pathUnzipped)
-console.log(pathProcessed)
+console.log(pathProcessedGray)
 
 IOhandler.unzip(zipFilePath, pathUnzipped)
     .then(()=> console.log("Extraction operation complete"))
@@ -28,17 +31,52 @@ IOhandler.unzip(zipFilePath, pathUnzipped)
 //          IOhandler.filterMyImage(path.join(__dirname,"unzipped",files[2]), pathProcessed + "/modified3.png", "sepia")
 // ]))
     .then((directoryFiles) => {
+        const run = async () => {
+            const askFilter = new AutoComplete({
+            name: 'filter',
+            message: 'Choose from available filters: ',
+            limit: 10,
+            initial: 0,
+            choices: ['grayscale','sepia',]
+            
+            });
+        const filter = await askFilter.run();
+        return filter
+        }
+        let filter = run()
+        return filter.then(filterType => {
+            return {'directoryFiles':directoryFiles, 'filterType':filterType}
+        }
+        )
+})
+    .then((imageConversionInfo) => {
+        console.log("imageConversionInfo", imageConversionInfo)
         let numFiles = fs.readdir(path.join(__dirname,"unzipped"), (err, files) =>{
             if(!err){
                 // console.log(files.length);
                 return files.length;
             }
         })
+        console.log("filtertype", imageConversionInfo.filterType)
+        let pathProcessed = (imageConversionInfo.filterType === "sepia") ? pathProcessedSepia : pathProcessedGray
+        console.log("path", pathProcessed)
 
         const ch = wt.createChannel(IOhandler.filterMyImage, numFiles);
-
-        for(let index in directoryFiles){
-            ch.add({pathIn: path.join(__dirname,"unzipped",directoryFiles[index]), pathOut: pathProcessed + `/modified${index}.png`, filterKind: "sepia", callback: ch.stop})
+        ch.on('message', (msg) => resolve(msg))
+        ch.on("done", (err, result) => {
+        if (err) {
+            console.error(err);
+        }
+        
+        console.log(result);
+        });
+        
+        ch.on("stop", () => {
+        console.log("channel is stop");
+        });
+        for(let index in imageConversionInfo.directoryFiles){
+            // ch.add({pathIn: path.join(__dirname,"unzipped",imageConversionInfo.directoryFiles[index]), pathOut: pathProcessed + `/modified${index}.png`, filterKind: imageConversionInfo.filterType, callback: ch.stop})
+            ch.add({pathIn: path.join(__dirname,"unzipped",imageConversionInfo.directoryFiles[index]), pathOut: pathProcessed + `/modified${index}.png`, filterKind: imageConversionInfo.filterType})
 
         }
         
