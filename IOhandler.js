778dffcd8fee4539a7486095d2531ad09cc6c010
omgitsjohn <syin18@my.bcit.ca>
2024-02-18 22:24:16 -0800

    moving code from main to iohandler

--- IOhandler.js
+++ IOhandler.js
@@ -8,44 +8,41 @@
  *
  */
 
-
 // const unzipper = require("unzipper"),
 // AdmZip = require("adm-zip"),
-
-const yauzl = require('yauzl-promise'),
+const wt = require("worker-thread");
+const yauzl = require("yauzl-promise"),
   fs = require("fs").promises,
-  { createReadStream, createWriteStream, readdir } = require("fs"), 
+  { createReadStream, createWriteStream, readdir } = require("fs"),
   PNG = require("pngjs").PNG;
 const path = require("path"),
-  { pipeline} = require("stream/promises"),
-  { Transform } = require('stream');
+  { pipeline } = require("stream/promises"),
+  { Transform } = require("stream");
 
-
-  const grayscaleHelper = function(idx) {
-  const gray = ( this.data[idx] +  this.data[idx+1] +  this.data[idx+2]) / 3
+const grayscaleHelper = function (idx) {
+  const gray = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
   this.data[idx] = gray;
   this.data[idx + 1] = gray;
   this.data[idx + 2] = gray;
-}
-  const sepiaHelper = function(idx) {
-        let red = this.data[idx];
-        let green = this.data[idx + 1];
-        let blue = this.data[idx + 2];
+};
+const sepiaHelper = function (idx) {
+  let red = this.data[idx];
+  let green = this.data[idx + 1];
+  let blue = this.data[idx + 2];
 
-        let sepiaRed = (red * 0.393) + (green * 0.769) + (blue * 0.189);
-        let sepiaGreen = (red * 0.349) + (green * 0.686) + (blue * 0.168);
-        let sepiaBlue = (red * 0.272) + (green * 0.534) + (blue * 0.131);
+  let sepiaRed = red * 0.393 + green * 0.769 + blue * 0.189;
+  let sepiaGreen = red * 0.349 + green * 0.686 + blue * 0.168;
+  let sepiaBlue = red * 0.272 + green * 0.534 + blue * 0.131;
 
-        this.data[idx] = sepiaRed < 255 ? sepiaRed : 255;
-        this.data[idx + 1] = sepiaGreen < 255 ? sepiaGreen : 255;
-        this.data[idx + 2] = sepiaBlue < 255 ? sepiaBlue : 255;
-  }
+  this.data[idx] = sepiaRed < 255 ? sepiaRed : 255;
+  this.data[idx + 1] = sepiaGreen < 255 ? sepiaGreen : 255;
+  this.data[idx + 2] = sepiaBlue < 255 ? sepiaBlue : 255;
+};
 
-  const filters = {
+const filters = {
   grayscale: grayscaleHelper,
-  sepia: sepiaHelper
-}
-   
+  sepia: sepiaHelper,
+};
 
 /**
  * Description: decompress file from given pathIn, write to given pathOut
@@ -55,95 +52,87 @@ const path = require("path"),
  * @return {promise}
  */
 
-
-
 const unzip = async (pathIn, pathOut) => {
-    const zip = await yauzl.open(pathIn);
-    try {
-        for await (const entry of zip) {
-            if (entry.filename.includes('/')) {
-                continue;
-            }
-            const readStream = await entry.openReadStream();
-            const writeStream = createWriteStream(
-                // path.join(pathOut, entry.fileName)
-                `${pathOut}/${entry.filename}`
-            );
-            await pipeline(readStream, writeStream);
-        }
-    } 
-    finally {
-      await zip.close();
+  const zip = await yauzl.open(pathIn);
+  try {
+    for await (const entry of zip) {
+      if (entry.filename.includes("/")) {
+        continue;
+      }
+      const readStream = await entry.openReadStream();
+      const writeStream = createWriteStream(
+        // path.join(pathOut, entry.fileName)
+        `${pathOut}/${entry.filename}`
+      );
+      await pipeline(readStream, writeStream);
     }
+  } finally {
+    await zip.close();
+  }
 };
-
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
-*
-* @param {string} path
-* @return {promise}
-*/
+ *
+ * @param {string} path
+ * @return {promise}
+ */
 const readDir = (dir) => {
   return new Promise((resolve, reject) => {
     readdir(dir, (err, files) => {
-      if(err)
-      reject(err);
-    else
-        resolve(files.filter((file)=> path.extname(file) === ".png"))
-    })
-  })
+      if (err) reject(err);
+      else resolve(files.filter((file) => path.extname(file) === ".png"));
+    });
+  });
 };
-
 
 //no longer used as the errorHandling for pipelines is deal with by stream/promise
 const errorHandler = (err) => {
   if (err) {
-    console.log("in error")
+    console.log("in error");
     console.log(err);
   }
 };
 
-
-
-const filterStream = function(imageFilter) {
-   for (let y = 0; y < this.height; y++) {
-      for (let x = 0; x < this.width; x++) {
-        let i = (this.width * y + x) << 2;
-        filters[imageFilter].call(this, i);
-      }
+const filterStream = function (imageFilter) {
+  for (let y = 0; y < this.height; y++) {
+    for (let x = 0; x < this.width; x++) {
+      let i = (this.width * y + x) << 2;
+      filters[imageFilter].call(this, i);
     }
-      this.pack();
   }
+  this.pack();
+};
 
 /**
  * Description: Read in png file by given pathIn,
  * convert to grayscale and write to given pathOut
-*
-* @param {string} filePath
-* @param {string} pathProcessed
-* @return {promise}
-*/
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
+ * @return {promise}
+ */
 
-const filterMyImage = ({pathIn, pathOut, filterKind, callback}) => {
+const filterMyImage = ({ pathIn, pathOut, filterKind, callback }) => {
   // return (
-  //   pipeline(createReadStream(pathIn), new PNG().on("parsed", function() { 
+  //   pipeline(createReadStream(pathIn), new PNG().on("parsed", function() {
   //     filterStream.call(this, filterKind)
-  //   }), 
+  //   }),
   //   createWriteStream(pathOut)
   //   ))
-    let promise  = pipeline(createReadStream(pathIn), new PNG().on("parsed", function() { 
-      filterStream.call(this, filterKind)
-    }), 
-    createWriteStream(pathOut));
+  let promise = pipeline(
+    createReadStream(pathIn),
+    new PNG().on("parsed", function () {
+      filterStream.call(this, filterKind);
+    }),
+    createWriteStream(pathOut)
+  );
 
-    promise.then(() => {
-      callback();
-      return promise
-    })
-    
+  promise.then(() => {
+    callback();
+    return promise;
+  });
 };
-
 
 // const filterMyImage = (pathIn, pathOut, filterKind) => {
 //   const transformStream = new Transform({
@@ -166,13 +155,49 @@ const filterMyImage = ({pathIn, pathOut, filterKind, callback}) => {
 //     createWriteStream(pathOut)
 //   );
 // };
+const convertImage = (imageConversionInfo) => {
+  const pathProcessedGray = path.join(__dirname, "grayscaled");
+  const pathProcessedSepia = path.join(__dirname, "sepia");
+  let numFiles = fs.readdir(path.join(__dirname, "unzipped"), (err, files) => {
+    if (!err) {
+      // console.log(files.length);
+      return files.length;
+    }
+  });
+  // console.log("filtertype", imageConversionInfo.filterType)
+  let pathProcessed =
+    imageConversionInfo.filterType === "sepia"
+      ? pathProcessedSepia
+      : pathProcessedGray;
+  // console.log("path", pathProcessed)
 
+  const ch = wt.createChannel(filterMyImage, numFiles);
+  ch.on("message", (msg) => resolve(msg));
+  // ch.on("done", (err, result) => {
+  //   if (err) {
+  //     console.error(err);
+  //   }
+  // });
+
+  for (let index in imageConversionInfo.directoryFiles) {
+    // ch.add({pathIn: path.join(__dirname,"unzipped",imageConversionInfo.directoryFiles[index]), pathOut: pathProcessed + `/modified${index}.png`, filterKind: imageConversionInfo.filterType, callback: ch.stop})
+    ch.add({
+      pathIn: path.join(
+        __dirname,
+        "unzipped",
+        imageConversionInfo.directoryFiles[index]
+      ),
+      pathOut: pathProcessed + `/modified${index}.png`,
+      filterKind: imageConversionInfo.filterType,
+    });
+  }
+};
 module.exports = {
   unzip,
   readDir,
   filterMyImage,
+  convertImage,
 };
-
 
 // const unzipStream = (file) => new Transform({
 //   transform(chunk, encoding, callback) {
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const wt = require("worker-thread");
-const {AutoComplete} = require('enquirer');
+const { AutoComplete } = require("enquirer");
 /*
  * Project: Milestone 1
  * File Name: main.js
@@ -15,71 +15,37 @@ const {AutoComplete} = require('enquirer');
 const IOhandler = require("./IOhandler");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
-const pathProcessedGray = path.join(__dirname, "grayscaled");
-const pathProcessedSepia = path.join(__dirname, "sepia");
-const readline = require('readline');
+const readline = require("readline");
 
-console.log(zipFilePath)
-console.log(pathUnzipped)
-console.log(pathProcessedGray)
+// console.log(zipFilePath);
+// console.log(pathUnzipped);
 
 IOhandler.unzip(zipFilePath, pathUnzipped)
-    .then(()=> console.log("Extraction operation complete"))
-    .then(()=> IOhandler.readDir(pathUnzipped))
-//     .then((files) => Promise.all([IOhandler.filterMyImage(path.join(__dirname,"unzipped",files[0]), pathProcessed + "/modified1.png", "sepia"), 
-//          IOhandler.filterMyImage(path.join(__dirname,"unzipped",files[1]), pathProcessed + "/modified2.png", "sepia"),
-//          IOhandler.filterMyImage(path.join(__dirname,"unzipped",files[2]), pathProcessed + "/modified3.png", "sepia")
-// ]))
-    .then((directoryFiles) => {
-        const run = async () => {
-            const askFilter = new AutoComplete({
-            name: 'filter',
-            message: 'Choose from available filters: ',
-            limit: 10,
-            initial: 0,
-            choices: ['grayscale','sepia',]
-            
-            });
-        const filter = await askFilter.run();
-        return filter
-        }
-        let filter = run()
-        return filter.then(filterType => {
-            return {'directoryFiles':directoryFiles, 'filterType':filterType}
-        }
-        )
-})
-    .then((imageConversionInfo) => {
-        console.log("imageConversionInfo", imageConversionInfo)
-        let numFiles = fs.readdir(path.join(__dirname,"unzipped"), (err, files) =>{
-            if(!err){
-                // console.log(files.length);
-                return files.length;
-            }
-        })
-        console.log("filtertype", imageConversionInfo.filterType)
-        let pathProcessed = (imageConversionInfo.filterType === "sepia") ? pathProcessedSepia : pathProcessedGray
-        console.log("path", pathProcessed)
-
-        const ch = wt.createChannel(IOhandler.filterMyImage, numFiles);
-        ch.on('message', (msg) => resolve(msg))
-        ch.on("done", (err, result) => {
-        if (err) {
-            console.error(err);
-        }
-        
-        console.log(result);
-        });
-        
-        ch.on("stop", () => {
-        console.log("channel is stop");
-        });
-        for(let index in imageConversionInfo.directoryFiles){
-            // ch.add({pathIn: path.join(__dirname,"unzipped",imageConversionInfo.directoryFiles[index]), pathOut: pathProcessed + `/modified${index}.png`, filterKind: imageConversionInfo.filterType, callback: ch.stop})
-            ch.add({pathIn: path.join(__dirname,"unzipped",imageConversionInfo.directoryFiles[index]), pathOut: pathProcessed + `/modified${index}.png`, filterKind: imageConversionInfo.filterType})
-
-        }
-        
-    })
-    .then(() => console.log("All images done!"))
-    .catch((err) => console.log("err", err))
+  .then(() => console.log("Extraction operation complete"))
+  .then(() => IOhandler.readDir(pathUnzipped))
+  //     .then((files) => Promise.all([IOhandler.filterMyImage(path.join(__dirname,"unzipped",files[0]), pathProcessed + "/modified1.png", "sepia"),
+  //          IOhandler.filterMyImage(path.join(__dirname,"unzipped",files[1]), pathProcessed + "/modified2.png", "sepia"),
+  //          IOhandler.filterMyImage(path.join(__dirname,"unzipped",files[2]), pathProcessed + "/modified3.png", "sepia")
+  // ]))
+  .then((directoryFiles) => {
+    const run = async () => {
+      const askFilter = new AutoComplete({
+        name: "filter",
+        message: "Choose from available filters: ",
+        limit: 10,
+        initial: 0,
+        choices: ["grayscale", "sepia"],
+      });
+      const filter = await askFilter.run();
+      return filter;
+    };
+    let filter = run();
+    return filter.then((filterType) => {
+      return { directoryFiles: directoryFiles, filterType: filterType };
+    });
+  })
+  .then((imageConversionInfo) => {
+    IOhandler.convertImage(imageConversionInfo);
+  })
+  .then(() => console.log("All images done!"))
+  .catch((err) => console.log("err", err));
